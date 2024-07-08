8b4d39fc5645581c8737de0c3a13f099811de1d3
ecimianog <ecimianog@gmail.com>
2024-07-08 23:16:08 +0200

    Plots mediapipe

--- headpose.py
+++ headpose.py
@@ -2,6 +2,15 @@ import cv2
 import numpy as np
 import mediapipe as mp
 from mediapipe.framework.formats import landmark_pb2
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+
+# Dividir los datos en conjuntos de entrenamiento y validación
+# pip install scikit-learn
+from sklearn.model_selection import train_test_split as sklearn_train_test_split
+import matplotlib.pyplot as plt
 
 # Configuracion de mediapipe
 mpFaceMesh = mp.solutions.face_mesh
@@ -19,7 +28,8 @@ drawingSpecD = mpDrawing.DrawingSpec(
     color=(255, 100, 0), thickness=3, circle_radius=1)
 drawingSpec = mpDrawing.DrawingSpec(
     color=(0, 0, 0), thickness=1, circle_radius=0)
-
+features = []
+labels = []
 # Definicion del identificador para dibujar los puntos según zonas de la cara
 EYEBROW_LANDMARKS = [70, 63, 105, 66, 107, 336, 296, 334, 293, 300]
 LIPS_LANDMARKS = [61, 146, 91, 181, 84, 17, 314, 405, 405, 321,
@@ -65,6 +75,8 @@ def hidemd():
 
 
 def lookFront(image):
+    global features
+    global labels
     faceOvalMarks = {}
     # Se prepara la imagen para procesarla
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
@@ -145,7 +157,12 @@ def lookFront(image):
                 if idx in FACE_OVAL_PASS:
                     ind = FACE_OVAL_PASS.index(idx)
                     faceOvalMarks[FACE_OVAL_PASS_ID[ind]] = lm
-
+            if len(features) < 300:
+                landmarks = np.array(
+                    [(lm.x, lm.y, lm.z) for lm in faceLandmarks.landmark]
+                )
+                features.append(landmarks.flatten())
+                labels.append(len(features))
             # Conversión a arrays numpy.
             face2d = np.array(face2d, dtype=np.float64)
             face3d = np.array(face3d, dtype=np.float64)
@@ -183,7 +200,6 @@ def lookFront(image):
             else:
                 msg = ''
                 valid = True
-
             # Si se muestra la ventana, se muestra el texto generado
             if showThis:
                 cv2.putText(image, msg, (20, 450),
@@ -205,8 +221,8 @@ def lookFront(image):
 
             # Se sale con Esc
             key = cv2.waitKey(1)
-            if key == 27:
-                break
+            if len(features) > 200 and key == 115:
+                train_test()
             # Envía resultado
             if valid:
                 return [True, faceOvalMarks]
@@ -215,3 +231,96 @@ def lookFront(image):
     # Si la imagen no tiene puntos detectados, se envía respuesta no satisfactoria
     else:
         return [False, 'Colóquese frente a la cámara']
+
+
+def train_test():
+
+    featuresNP = np.array([f for f in features if f is not None])
+    print(2, featuresNP)
+    X_train, X_val, y_train, y_val = sklearn_train_test_split(
+        featuresNP, labels, train_size=0.2, random_state=42)
+    # Convertir a arrays de NumPy
+    X_train = np.array(X_train)
+    X_val = np.array(X_val)
+    y_train = np.array(y_train)
+    y_val = np.array(y_val)
+    printTA = f"X_train:\n{X_train}"
+    printTB = f"X_val:\n{X_val}"
+    printTC = f"y_train:\n{y_train}"
+    printTD = f"y_val:\n{y_val}"
+    printings = [printTA, printTB, printTC, printTD]
+
+    # Verificar las formas de los arrays
+    printa = "X_train shape:", X_train.shape
+    printb = "y_train shape:", y_train.shape
+    printc = "X_val shape:", X_val.shape
+    printd = "y_val shape:", y_val.shape
+    print(printa)
+    print(printb)
+    print(printc)
+    print(printd)
+    printings.append(printa)
+    printings.append(printb)
+    printings.append(printc)
+    printings.append(printd)
+    # Definir el modelo
+    model = Sequential([
+        Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
+        Dropout(0.5),
+        Dense(64, activation='relu'),
+        Dropout(0.5),
+        Dense(1, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # Entrenar el modelo y guardar el historial
+    history = model.fit(X_train, y_train, epochs=1000,
+                        validation_data=(X_val, y_val))
+
+    # Guardar las pérdidas de entrenamiento y validación
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    train_accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+    printe = 'Training loss: {}'.format(train_loss[-1])
+    printf = 'Validation loss: {}'.format(val_loss[-1])
+    printg = 'Training accuracy: {}'.format(train_accuracy[-1])
+    printh = 'Validation accuracy: {}'.format(val_accuracy[-1])
+    print(printe)
+    print(printf)
+    print(printg)
+    print(printh)
+    printings.append(printe)
+    printings.append(printf)
+    printings.append(printg)
+    printings.append(printh)
+    with open('trainingCalc.dat', 'w') as f:
+        for l in printings:
+            f.write(f"{l}\n")
+    plot_history(train_loss, val_loss, train_accuracy, val_accuracy)
+
+
+def plot_history(train_loss, val_loss, train_accuracy, val_accuracy):
+    # Gráfico de Training Loss y Validation Loss
+    plt.figure(figsize=(10, 6))
+    plt.plot(train_loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Training Loss vs. Validation Loss')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    # Gráfico de Training Accuracy y Validation Accuracy
+    plt.figure(figsize=(10, 6))
+    plt.plot(train_accuracy, label='Training Accuracy')
+    plt.plot(val_accuracy, label='Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Training Accuracy vs. Validation Accuracy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
